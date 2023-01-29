e6ab9fa085a97b21aba857c54ad262ef15e988ba
Gabriel Shoyombo <gabrielshoyombo2002@gmail.com>
2023-01-29 09:23:10 +0100

    Built a static version of the app

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+//import image from "../src/assets/Images/Products/image.png";
+</script>
+
+<template>
+  <div class="container">
+    <h2 class="title">UpVote!</h2>
+    <div class="product-container">
+      <div class="product">
+        <div class="product-img">
+          <img src="../src/assets/Images/Products/image.png" alt="product" />
+        </div>
+        <div class="product-content">
+          <div class="product-title">
+            <span class="left-content">
+              <span class="prod-title">Yellow Page</span>
+              <span class="product-rank">#4</span>
+            </span>
+            <span class="product-vote">10</span>
+          </div>
+          <div class="product-desc">
+            On-demand sand castle construction expertise.
+          </div>
+          <div class="product-owner">
+            Submitted by:
+            <span class="product-owner-img">
+              <img src="../src/assets/Images/Avatars/daniel.jpg" alt="owner" />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
